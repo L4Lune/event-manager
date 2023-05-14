da5a71f5f7bde552acacf837fa70c21207ca0fb7
Jack Roudebush <roudebushjack@gmail.com>
2023-05-13 21:37:01 -0400

    Update erb template and legislators method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,3 +1,4 @@
+require 'erb'
 require 'csv'
 require 'google/apis/civicinfo_v2'
 
@@ -6,16 +7,11 @@ def legislators_by_zipcode(zip)
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials
-
-    legislator_names = legislators.map(&:name)
-
-    legislator_names.join(", ")
+    ).officials
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
@@ -33,6 +29,9 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
   name = row[:first_name]
 
@@ -40,5 +39,6 @@ contents.each do |row|
 
   legislators = legislators_by_zipcode(zipcode)
   
-  puts "#{name} #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+  puts form_letter
 end
